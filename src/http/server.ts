49d6eb8d205e392423ebf45794aa8d8b800d0e29
guuilhermessantos <guuilhermessantos@gmail.com>
2024-09-12 21:00:36 -0300

    finish server in.orbit

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,22 +1,34 @@
 import fastify from 'fastify'
+import {
+	serializerCompiler,
+	validatorCompiler,
+	type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
 import { createGoal } from '../functions/create-goal'
 import z from 'zod'
+import { getWeekPendingGoals } from '../functions/get-week-pending-goals'
+import { createGoalCompletion } from '../functions/create-goal-completion'
 
-const app = fastify()
+import fastifyCors from '@fastify/cors'
+import { createGoalRoute } from './routes/create-goal'
+import { createCompletionRoute } from './routes/create-completion'
+import { getPendingGoalsRoute } from './routes/get-pending-goals'
+import { getWeekSummaryRoute } from './routes/get-week-summary'
 
-app.post('/goals', async (req, res) => {
-  const createGoalSchema = z.object({
-    title: z.string(),
-    desiredWeeklyFrequency: z.number().int().min(1).max(7),
-  })
-  const body = createGoalSchema.parse(req.body)
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
-  await createGoal({
-    title: body.title,
-    desiredWeeklyFrequency: body.desiredWeeklyFrequency,
-  })
+app.register(fastifyCors, {
+	origin: '*',
 })
 
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(createGoalRoute)
+app.register(createCompletionRoute)
+app.register(getPendingGoalsRoute)
+app.register(getWeekSummaryRoute)
+
 app.listen({ port: 3333 }).then(() => {
-  console.log('HTTP server running 🚀')
+	console.log('HTTP server running 🚀')
 })
